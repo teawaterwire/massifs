b4fe4a0b5c4af107c1224d8a1c4816191311fdb5
Theophile Villard <theophile.villard@gmail.com>
2017-06-28 17:40:51 +0200

    Ça prend du temps bordel

---
+++ js/fire.js
@@ -0,0 +1,10 @@
+var config = {
+  apiKey: "AIzaSyAVa2NScfcIVtQkWnycchYrCk0fLn-OaU0",
+  authDomain: "massigs-1d44d.firebaseapp.com",
+  databaseURL: "https://massigs-1d44d.firebaseio.com",
+  projectId: "massigs-1d44d",
+  storageBucket: "",
+  messagingSenderId: "954020848727"
+};
+
+firebase.initializeApp(config);
